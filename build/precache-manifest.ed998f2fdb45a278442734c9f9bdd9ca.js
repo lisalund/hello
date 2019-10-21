6b55b8375bbecef35e602cd489825db499c4e6fc
lisalund <lislund@kth.se>
2019-10-21 16:44:06 +0200

    trying to get router to work

---
+++ build/precache-manifest.ed998f2fdb45a278442734c9f9bdd9ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "529084a6c0c5be7b60a4",
+    "url": "/hello/static/js/runtime~main.529084a6.js"
+  },
+  {
+    "revision": "161e2374d1d6dcb2653e",
+    "url": "/hello/static/js/main.161e2374.chunk.js"
+  },
+  {
+    "revision": "cba07c87f30469f73914",
+    "url": "/hello/static/js/2.cba07c87.chunk.js"
+  },
+  {
+    "revision": "161e2374d1d6dcb2653e",
+    "url": "/hello/static/css/main.3cc8d835.chunk.css"
+  },
+  {
+    "revision": "cba07c87f30469f73914",
+    "url": "/hello/static/css/2.8d362fe8.chunk.css"
+  },
+  {
+    "revision": "bb486cdd3ac8dc88b43981b9a2f61819",
+    "url": "/hello/index.html"
+  }
+];
