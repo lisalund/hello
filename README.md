9aa762b1697d4aae58cb2a5e8d42944a66993088
lisalund <lislund@kth.se>
2019-10-21 15:09:03 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# PORTFOLIO-APP 
+[![Build Status](https://travis-ci.org/dbarochiya/me.svg?branch=master)](https://travis-ci.org/dbarochiya/me)
+
+This is the source code for my portfolio page. It was based on Dhurv Barochiya's blog post on how to create a portfolio using React.
+    
+##### deployed app : https://lisalund.github.io/hello/
---
+++ portfolio-app/src/components/projects.jsx
@@ -0,0 +1,161 @@
+import React, { Component } from 'react'
+import { Card, CardColumns } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+
+export default class Projects extends Component {
+  render() {
+      let sparkleCard = (<Card border="light">
+          <Card.Img variant="top" src="images/projects/sparkleCard.png" />
+          <Card.Body className="projectCard">
+              <Card.Title>Sparkle Food Diary</Card.Title>
+              <Card.Text>
+                  A prototype for a food-tracking app meant for people in remission from an eating disorder
+              </Card.Text>
+          </Card.Body>
+      </Card>);
+
+
+  	return(
+  		<div>
+			<section className="colorlib-work" data-section="projects">
+                <div className="colorlib-narrow-content">
+					<div className="row">
+						<div className="col-md-6 col-md-offset-3 col-md-pull-3">
+							<span className="heading-meta">My Work</span>
+							<h2 className="colorlib-heading">Projects</h2>
+						</div>
+					</div>
+						<CardColumns>
+                            <Link to={'/projects/sparkle'}>{sparkleCard}</Link>
+                            <Card>
+                                <Card.Img variant="top" src="images/projects/underConstruction.png" />
+                                <Card.Body>
+                                    <Card.Title>More coming soon</Card.Title>
+                                    <Card.Text>
+                                        This portfolio is under construction, more of my projects will be added shortly
+                                    </Card.Text>
+                                </Card.Body>
+                            </Card>
+
+						</CardColumns>
+					<div>
+
+					</div>
+				</div>
+			</section>
+		</div>
+	)
+
+    /*return (
+      <div>
+				<section className="colorlib-work" data-section="projects">
+					<div className="colorlib-narrow-content">
+						<div className="row">
+							<div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
+								<span className="heading-meta">My Work</span>
+								<h2 className="colorlib-heading animate-box">Recent Projects</h2>
+							</div>
+						</div>
+						<div className="row">
+							<div className="col-md-4 animate-box" data-animate-effect="fadeInLeft">
+								<div className="project" style={{backgroundImage: 'url(images/img-1.jpg)'}}>
+									<div className="desc">
+										<div className="con">
+											<h3><a href="work.html">Work 01</a></h3>
+											<span>Website</span>
+											<p className="icon">
+												<span><a href="#"><i className="icon-share3" /></a></span>
+												<span><a href="#"><i className="icon-eye" /> 100</a></span>
+												<span><a href="#"><i className="icon-heart" /> 49</a></span>
+											</p>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div className="col-md-4 animate-box" data-animate-effect="fadeInRight">
+								<div className="project" style={{backgroundImage: 'url(images/img-2.jpg)'}}>
+									<div className="desc">
+										<div className="con">
+											<h3><a href="work.html">Work 02</a></h3>
+											<span>Animation</span>
+											<p className="icon">
+												<span><a href="#"><i className="icon-share3" /></a></span>
+												<span><a href="#"><i className="icon-eye" /> 100</a></span>
+												<span><a href="#"><i className="icon-heart" /> 49</a></span>
+											</p>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div className="col-md-4 animate-box" data-animate-effect="fadeInTop">
+								<div className="project" style={{backgroundImage: 'url(images/img-3.jpg)'}}>
+									<div className="desc">
+										<div className="con">
+											<h3><a href="work.html">Work 03</a></h3>
+											<span>Illustration</span>
+											<p className="icon">
+												<span><a href="#"><i className="icon-share3" /></a></span>
+												<span><a href="#"><i className="icon-eye" /> 100</a></span>
+												<span><a href="#"><i className="icon-heart" /> 49</a></span>
+											</p>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div className="col-md-4 animate-box" data-animate-effect="fadeInBottom">
+								<div className="project" style={{backgroundImage: 'url(images/img-4.jpg)'}}>
+									<div className="desc">
+										<div className="con">
+											<h3><a href="work.html">Work 04</a></h3>
+											<span>Application</span>
+											<p className="icon">
+												<span><a href="#"><i className="icon-share3" /></a></span>
+												<span><a href="#"><i className="icon-eye" /> 100</a></span>
+												<span><a href="#"><i className="icon-heart" /> 49</a></span>
+											</p>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div className="col-md-4 animate-box" data-animate-effect="fadeInLeft">
+								<div className="project" style={{backgroundImage: 'url(images/img-5.jpg)'}}>
+									<div className="desc">
+										<div className="con">
+											<h3><a href="work.html">Work 05</a></h3>
+											<span>Graphic, Logo</span>
+											<p className="icon">
+												<span><a href="#"><i className="icon-share3" /></a></span>
+												<span><a href="#"><i className="icon-eye" /> 100</a></span>
+												<span><a href="#"><i className="icon-heart" /> 49</a></span>
+											</p>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div className="col-md-4 animate-box" data-animate-effect="fadeInRight">
+								<div className="project" style={{backgroundImage: 'url(images/img-6.jpg)'}}>
+									<div className="desc">
+										<div className="con">
+											<h3><a href="work.html">Work 06</a></h3>
+											<span>Web Design</span>
+											<p className="icon">
+												<span><a href="#"><i className="icon-share3" /></a></span>
+												<span><a href="#"><i className="icon-eye" /> 100</a></span>
+												<span><a href="#"><i className="icon-heart" /> 49</a></span>
+											</p>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div className="row">
+							<div className="col-md-12 animate-box">
+								<p><a href="#" className="btn btn-primary btn-lg btn-load-more">Load more <i className="icon-reload" /></a></p>
+							</div>
+						</div>
+					</div>
+				</section>
+      </div>
+    )*/
+  }
+}
