bd50b3faead44e819fd235356d901fb17feaf614
lisalund <lislund@kth.se>
2019-10-22 20:03:47 +0200

    Updates

--- precache-manifest.8acfe504b96852129c36a399c450d3e8.js
+++ precache-manifest.8acfe504b96852129c36a399c450d3e8.js
@@ -4,23 +4,23 @@ self.__precacheManifest = [
     "url": "/hello/static/js/runtime~main.529084a6.js"
   },
   {
-    "revision": "e71dfdb3b258e629dc7a",
-    "url": "/hello/static/js/main.e71dfdb3.chunk.js"
+    "revision": "7da31a7357fca53da651",
+    "url": "/hello/static/js/main.7da31a73.chunk.js"
   },
   {
-    "revision": "cba07c87f30469f73914",
-    "url": "/hello/static/js/2.cba07c87.chunk.js"
+    "revision": "bccae75207b469d06488",
+    "url": "/hello/static/js/2.bccae752.chunk.js"
   },
   {
-    "revision": "e71dfdb3b258e629dc7a",
+    "revision": "7da31a7357fca53da651",
     "url": "/hello/static/css/main.3cc8d835.chunk.css"
   },
   {
-    "revision": "cba07c87f30469f73914",
+    "revision": "bccae75207b469d06488",
     "url": "/hello/static/css/2.8d362fe8.chunk.css"
   },
   {
-    "revision": "9d7c8a0134331885b54c6394595c0385",
+    "revision": "174e654cbb7b5e97850ad8944b37683f",
     "url": "/hello/index.html"
   }
 ];
